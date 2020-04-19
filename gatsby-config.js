d896a508eb4c1b35b47363ae1227d6538b25230f
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-04-19 17:51:37 +0530

    Tried graphqil filesystem query and printed  on  page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: `Pandas Eating Lots`,
+    description: `Example site to learn gatsby in and out`
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,

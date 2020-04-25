d11b268815cd4b66d6cb5d0ba77a89f37342eee3
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-04-25 17:47:49 +0530

    Now tag list page is done

--- src/templates/category-post.js
+++ src/templates/category-post.js
@@ -1,10 +1,43 @@
 import React from "react";
+import Layout from "../components/layout/Layout";
+import PostList from '../components/posts/PostList';
 
-const CategoryPost = () => {
-
-    return(
-        <h1>Category Post</h1>
+const CategoryPost = ({ data }) => {
+    console.log(data);
+    return (
+        <Layout>
+            <div>
+                <PostList data={data} />
+            </div>
+        </Layout>
     )
 };
 
 export default CategoryPost;
+
+export const query = graphql`
+  query($category: String!) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { category: { in: [$category] } } }
+      ) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            thumbnail
+            tags
+            category
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
